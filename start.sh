cd293a8c3c5a2158877efae33bab2b22f956a79d
Francisco Aranda <frascuchon@users.noreply.huggingface.co>
2024-10-08 12:35:42 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Preset oauth env vars based on injected space variables.
+# See https://huggingface.co/docs/hub/en/spaces-oauth#create-an-oauth-app
+export OAUTH2_HUGGINGFACE_CLIENT_ID=$OAUTH_CLIENT_ID
+export OAUTH2_HUGGINGFACE_CLIENT_SECRET=$OAUTH_CLIENT_SECRET
+export OAUTH2_HUGGINGFACE_SCOPE=$OAUTH_SCOPES
+
+# Set the space creator name as username if no name is provided, if the user is not found, use the provided space author name
+# See https://huggingface.co/docs/hub/en/spaces-overview#helper-environment-variables for more details
+DEFAULT_USERNAME=$(curl -L -s https://huggingface.co/api/users/${SPACES_CREATOR_USER_ID}/overview | jq -r '.user' || echo "${SPACE_AUTHOR_NAME}")
+export USERNAME="${USERNAME:-$DEFAULT_USERNAME}"
+DEFAULT_PASSWORD=$(pwgen -s 16 1)
+export PASSWORD="${PASSWORD:-$DEFAULT_PASSWORD}"
+
+# These lines only make sense if the container is running in a Hugging Face Spaces environment with persistent storage
+
+# 1. Create a backup of the existing persistent storage files once. If something goes wrong, we can restore the files
+# from the zero-backup directory
+if [ ! -d /data/backups/argilla ]; then
+  mkdir -p /data/backups/argilla
+  cp -r /data/argilla/* /data/backups/argilla
+fi
+
+# 2. Copy the backup file to the correct location
+cp -r /data/argilla/backup/* /data/* || true
+# 3. Copy all the persistent storage files to the correct location
+cp -r /data/argilla/* $ARGILLA_HOME_PATH
+
+honcho start
