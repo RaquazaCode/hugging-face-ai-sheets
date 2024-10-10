386b73d226443ef083a6dab88f9925e3e4165d55
Francisco Aranda <frascuchon@users.noreply.huggingface.co>
2024-10-10 13:24:46 +0000

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+# Preset oauth env vars based on injected space variables.
+# See https://huggingface.co/docs/hub/en/spaces-oauth#create-an-oauth-app
+export OAUTH2_HUGGINGFACE_CLIENT_ID=$OAUTH_CLIENT_ID
+export OAUTH2_HUGGINGFACE_CLIENT_SECRET=$OAUTH_CLIENT_SECRET
+export OAUTH2_HUGGINGFACE_SCOPE=$OAUTH_SCOPES
+
+# Set the space creator name as username if no name is provided, if the user is not found, use the provided space author name
+# See https://huggingface.co/docs/hub/en/spaces-overview#helper-environment-variables for more details
+DEFAULT_USERNAME=$(curl -L -s https://huggingface.co/api/users/${SPACES_CREATOR_USER_ID}/overview | jq -r '.user' || echo "${SPACE_AUTHOR_NAME}")
+export USERNAME="${USERNAME:-$DEFAULT_USERNAME}"
+
+DEFAULT_PASSWORD=$(pwgen -s 16 1)
+export PASSWORD="${PASSWORD:-$DEFAULT_PASSWORD}"
+
+if [ ! -d /data/argilla/backup ]; then
+  echo "Initializing backup folder..."
+  mkdir -p /data/argilla
+
+  # if exists the db file, copy it to the backup folder and rename it
+  if [ -f /data/argilla/argilla.db ]; then
+    echo "Found argilla.db file, moving it to the backup folder..."
+    cp /data/argilla/argilla.db /data/argilla/backup || true
+  fi
+
+  # if exists the server id file, copy it to the argilla folder
+  if [ -f /data/argilla/server_id.dat ]; then
+    echo "Found server_id.dat file, moving it to the backup folder..."
+    cp /data/argilla/server_id.dat /data/argilla/backup || true
+  fi
+
+else
+  echo "Backup folder already exists..."
+fi
+
+# Copy the backup files to the argilla folder
+echo "Restoring files from backup folder..."
+python ./restore_argilla_backup.py
+
+echo "Starting processes..."
+honcho start
